74cb67aa548c31c7b6637595c2a31cf804f95944
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-18 22:43:24 -0600

    Completed homework assignment

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //console.log("We are alive!");
 
+
 //Part 1: 
 //1. The difference between interpolation and concatenation is that concatenation 
 //adds strings together and interpolation is a template that strings new stuff together.
@@ -23,6 +24,109 @@
 //that you want to know.
 
 //7. Tab completion makes typing a lot faster! It saves time as well as helps with typo's.
+
+
+
+//Part 2:
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(a < b, b < c);
+console.log(a === a, a < d);
+console.log(e === 'Kevin');
+console.log(48 =='48');
+
+
+
+//Part 3:
+//1. It is an infinite loop because it is not specified where it will start or where it
+//will end. I think it is an infinite loop. Missing num++
+
+//2. It is an infinite loop because it also does not say where it will start or end. 
+// Missing num++
+
+//3. 
+let letters = "A"; //every time there is the word "letters" the value is "A"
+let i = 0; //every time there is a letter "i" the value is 0
+
+while (i < 20) { // 0 is less than 20 which is a true so now it goes to the next
+	letters += "A"; //"A" is plus or equal to "A", which is true so go on to next
+	i++; //the loop is done and can stop
+}
+
+console.log(letters); //equals true based on the code before
+
+//the expected result was wrong. It looks like the answer was "A" twenty (or twenty one) times
+
+
+
+//Part 4
+//1. "for" loops everything is done inside the loop
+//"while" loops have to be incremented before the loop and also it executes if the condition is true
+
+//2. 
+for (let i = 0; i <=999; i++){
+	console.log('The number is ${i}')
+}
+
+//3. 
+//The first part of the control statement is: the variable where our loop will start 
+//The second part of the control statement is: is saying which condition the loop will run to
+//The third part of the control statement is: increases the value each time till the end, controlling the stop of the loop
+
+//4. 
+for (let i = 999; i >=0; i--){
+	console.log('The number is ${i}')
+}
+
+//5. 
+for (let i = 0; i <=10; i++){
+	console.log('The value of i is: ${i} of 10');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
