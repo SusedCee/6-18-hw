f41374b6a8d42f2f500cacd65e92445b10858012
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-18 21:23:29 -0600

    Completed homework assignment

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//console.log("We are alive!");
+
+//Part 1: 
+//1. The difference between interpolation and concatenation is that concatenation 
+//adds strings together and interpolation is a template that strings new stuff together.
+
+//2. DRY stand for "Don't repeat yourself". We should pay attention to it because it makes 
+//our code cleaner and helps so we don't have broken codes as well as saves time. While 
+//loops and regular loops help us with dry code.
+
+//3. The difference between declaring a variable and assigning a value to a variable is that 
+//declaring a variable means that memory is put for the variable and assigning a value to a 
+//variable is when a value is assigned to the variable. The value in a variable can change so
+//defining the variable means what the value is at the end of an operation.
+
+//4. Const is when you want to be strict about not duplicating variables that are exactly 
+//the same. Let allows you to redefine a variable.
+
+//5. A parent directory is the directory in which the folder or current directory
+//you are working on (or thinking of) lives in. 
+
+//6. One way to find out what a command does in Unix is to type "man" and then the command
+//that you want to know.
+
+//7. Tab completion makes typing a lot faster! It saves time as well as helps with typo's.
+
+
+
+
+
